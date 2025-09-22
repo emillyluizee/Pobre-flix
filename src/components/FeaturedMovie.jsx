b21b6d4fcf87cbf207eb8f2266d6072ebd821d71
Emilly Luize <emillyluize56@gmail.com>
2025-09-22 14:54:36 -0300

    Adicionando mais filmes e séries

--- src/components/FeaturedMovie.jsx
+++ src/components/FeaturedMovie.jsx
@@ -6,7 +6,7 @@ const FeaturedMovie = () => {
     const movie = {
         title: 'LUCIFER',
         description: 'conta a história de **Lucifer Morningstar**, o Senhor do Inferno, que, cansado da sua vida, abdica do trono e vai viver para Los Angeles, onde se torna dono de uma boate de luxo e começa a colaborar com a detetive Chloe Decker, ajudando a resolver crimes e a combater seres sobrenaturais, enquanto lida com a sua própria família e a sua jornada de autodescoberta.',
-        imageUrl: 'https://placehold.co/600x400/000000/FFFFFF?text=Imagem+do+Elenco'
+        imageUrl: 'https://s2.glbimg.com/UBnybHLru0HDEq-_NYxlFWeSbQM=/top/e.glbimg.com/og/ed/f/original/2021/08/04/image_AgxCsTX.png'
     };
 
     return (
